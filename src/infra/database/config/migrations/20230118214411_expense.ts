ee8be9ad1b5d3b4169a1f3795e8c7b4a724ee74f
Victor Heshiki <victor_heshiki@hotmail.com>
2023-01-18 21:45:47 -0300

    refactor: change types of table

--- src/infra/database/config/migrations/20230118214411_expense.ts
+++ src/infra/database/config/migrations/20230118214411_expense.ts
@@ -5,8 +5,8 @@ export async function up (knex: Knex): Promise<void> {
     await knex.schema.createTable('expense', (table) => {
         table.increments("id").primary()
         table.decimal("value", 14, 2).notNullable()
-        table.date("date").notNullable()
-        table.string("description").notNullable()
+        table.datetime("date").notNullable()
+        table.text("description").notNullable()
         table.integer("typePaymentId").notNullable()
         table.integer("categoryId").notNullable()
     })
