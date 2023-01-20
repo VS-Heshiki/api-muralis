72404239e11b934a5700c410d2b13b6b1b504278
Victor Heshiki <victor_heshiki@hotmail.com>
2023-01-20 18:09:40 -0300

    fix: name of method

--- src/infra/database/mysql/expenseMySqlRepository.ts
+++ src/infra/database/mysql/expenseMySqlRepository.ts
@@ -1,7 +1,7 @@
 import { AddExpenseRepository, LoadExpenseRepository } from '@/data/repository'
 import { KnexHelper } from '@/infra/database/helper/knexHelper'
 
-export class ExpensePostgresRepository implements AddExpenseRepository, LoadExpenseRepository {
+export class ExpenseMySqlRepository implements AddExpenseRepository, LoadExpenseRepository {
     async add (params: AddExpenseRepository.Params): Promise<void> {
         const props = KnexHelper.map(params)
         await KnexHelper.add(props, 'expense')
