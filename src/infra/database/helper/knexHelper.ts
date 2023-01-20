6a4f2d8bbebfd93a735b4cf6f9c98ec1a4931073
Victor Heshiki <victor_heshiki@hotmail.com>
2023-01-20 17:04:23 -0300

    feat: return id on response

--- src/infra/database/helper/knexHelper.ts
+++ src/infra/database/helper/knexHelper.ts
@@ -1,11 +1,11 @@
-import { AddExpense } from '@/domain/features'
 import knex from '@/infra/database/config/knexconfig'
+import { AddExpense } from '@/domain/features'
 import moment from 'moment'
 
 
 export const KnexHelper = {
     format (date: Date): Date {
-        const momentObj = moment(date).format("DD/MM/YYYY")
+        const momentObj = moment(date).format('DD-MM-YYYY')
         const formatDate = moment(momentObj).toDate()
         return formatDate
     },
@@ -15,6 +15,11 @@ export const KnexHelper = {
         const props = [params]
         const result = props.map((a) => ({ ...a, date: objDate }))
         return result
+    },
+
+    async getId (table: string): Promise<any> {
+        const resolve = await knex.raw(`SELECT * FROM ${ table } WHERE id = (SELECT LAST_INSERT_ID())`)
+        return resolve[0]
     },
 
     async add (expense: AddExpense.Params, table: string): Promise<void> {
